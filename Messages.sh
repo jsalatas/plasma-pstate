c94402df020dda1842df17aead92b08368de275d
frankenfruity <frankenfruity@protonmail.com>
2021-08-20 12:48:32 -0400

    Fix cmake package name

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_org.kde.plasma.pstate.pot
+$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_gr.ictpro.jsalatas.plasma.pstate.pot
